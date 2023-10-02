c47d7a8b3d5227cd9f4c7734f9fddd29632b2191
Ivan Gil <igil.dev@gmail.com>
2023-10-02 16:17:20 -0400

    Login page

--- src/features/authentication/LoginForm.jsx
+++ src/features/authentication/LoginForm.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import Button from "../../ui/Button";
+
 import Form from "../../ui/Form";
 import Input from "../../ui/Input";
 import FormRowVertical from "../../ui/FormRowVertical";
